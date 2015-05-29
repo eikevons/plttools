03a16a3f64846a0c7eaa381a25646a99347782a5
Jan Eike von Seggern <eike.seggern@sevenval.com>
2015-05-29 15:50:52 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from distutils.core import setup
 
 setup(name='plttools',
-      version='0.0.1',
-      description='Tools to read and process CCD images',
+      version='1.0',
+      description='Small tools for matplotlib',
       author='Jan Eike von Seggern',
-      author_email='jan.eike.von.seggern@desy.de',
+      author_email='eikevons@yahoo.de',
       packages=['plttools'],
       requires=['numpy', 'matplotlib']
       )
